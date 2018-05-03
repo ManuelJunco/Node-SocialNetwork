b3ccb564888d8ce5a371d3f4199a595ba0cc59ed
uo252473@uniovi.es <uo252473@uniovi.es>
2018-05-03 10:29:09 +0200

    Registro hecho

--- SocialNetwork/app.js
+++ SocialNetwork/app.js
@@ -1,7 +1,33 @@
+/* FRAMEWORK DECLARATION */
 var express = require('express');
 var app = express();
 
+/* GLOBAL VARIABLES  - MODULES */
+var expressSession = require('express-session'); /* express-session installed */
+app.use(expressSession({
+    secret: 'abcdefg',
+    resave: true,
+    saveUninitialized: true
+}));
+var crypto = require('crypto'); /* not installed - included in core */
+var swig = require('swig'); /* swig installed */
+var mongo = require('mongodb'); /* mongo 2.2.33 installed */
+var bodyParser = require('body-parser'); /* body-parser installed */
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+var gestorBD = require("./modules/gestorBD.js");
+gestorBD.init(app,mongo);
 
+/* GLOBAL VARIABLES - APP && OTHERS */
+app.use(express.static('public'));
+app.set('db','mongodb://admin:SocialNetworkAdmin@ds111370.mlab.com:11370/socialnetwork');
+app.set('clave','abcdefg');
+app.set('crypto',crypto);
+
+/* CONTROLLERS*/
+require("./routes/rusuarios.js")(app, swig, gestorBD);
+
+/* MESSAGES AND PORT */
 app.listen(8081, function(){
 	console.log("Servidor activo");
 });
---
+++ SocialNetwork/routes/rusuarios.js
@@ -0,0 +1,67 @@
+module.exports = function (app, swig, gestorBD) {
+
+
+
+    /* Return the login html */
+    app.get("/identificarse", function (req, res) {
+        var respuesta = swig.renderFile('views/login.html', {});
+        res.send(respuesta);
+    });
+
+
+    /* Return the signup html */
+    app.get("/registrarse", function (req, res) {
+        var respuesta = swig.renderFile('views/signup.html', {});
+        res.send(respuesta);
+    });
+
+
+    /* Receive the post req from signup html */
+    app.post("/usuario", function (req, res) {
+        /*First . Check if password == confirmPassword*/
+        if (req.body.password != req.body.confirmPassword) {
+            res.redirect("/registrarse?mensaje=La password y su confirmación deben ser iguales&tipoMensaje=alert-danger");
+        } else {
+            var re = /\S+@\S+/;
+            /* Second - validate email */
+            if (!re.test(req.body.email)) {
+                res.redirect("/registrarse?El email no es válido&tipoMensaje=alert-danger");
+            } else {
+                /* Third - Check length */
+                if (req.body.password.length < 7) {
+                    res.redirect("/registrarse?mensaje=Password demasiado corta&tipoMensaje=alert-danger");
+                } else {
+                    /* Fourth - Check if already exists */
+                    var criterio = {
+                        email: req.body.email
+                    };
+                    gestorBD.obtenerUsuarios(criterio, function (usuarios) {
+                        if (usuarios == null) {
+                            res.redirect("/registrarse?mensaje=Error al registrar al usuario&tipoMensaje=alert-danger");
+                        } else {
+                            if (usuarios[0].email == req.body.email) {
+                                res.redirect("/registrarse?mensaje=El email ya existe&tipoMensaje=alert-danger");
+                            } else {
+                                /* Adding user */
+                                var seguro = app.get("crypto").createHmac('sha256', app.get('clave'))
+                                    .update(req.body.password).digest('hex');
+                                var usuario = {
+                                    email: req.body.email,
+                                    password: seguro
+                                };
+                                gestorBD.insertarUsuario(usuario, function (id) {
+                                    if (id == null) {
+                                        res.redirect("/registrarse?mensaje=Error al registrar usuario&tipoMensaje=alert-danger");
+                                    } else {
+                                        res.redirect("/identificarse?mensaje=Nuevo usuario registrado");
+                                    }
+                                });
+                            }
+                        }
+                    });
+                }
+            }
+        }
+    });
+
+};
